0ea84da5ec1bfee8b3c14217dca2570ec1ad5f21
yudongrao <yudong.rao@genesys.com>
2019-10-16 00:35:40 -0400

    final

---
+++ prog3/interaction.js
@@ -0,0 +1,241 @@
+var dist = 0.01;
+var rotation = 0.01;
+var rotationAngle = Math.PI / 20;
+
+function onKeydown(e) {
+    var numOfModels = inputTriangles.length;
+    var key = e.key;
+    switch(key) {
+        // Part 3: interactively change view
+        // a and d — translate view left and right along view X
+        // w and s — translate view forward and backward along view Z
+        // q and e — translate view up and down along view Y
+        // A and D — rotate view left and right around view Y (yaw)
+        // W and S — rotate view forward and backward around view X (pitch)
+        case "a":
+            eye[0] -= dist;
+            lookCenter[0] -= rotation;
+            console.log("translate view left along view X");
+            break;
+        case "d":
+            eye[0] += dist;
+            lookCenter[0] += rotation;
+            console.log("translate view right along view X");
+            break;
+        case "w":
+            eye[2] += dist;
+            lookCenter[2] += rotation;
+            console.log("translate view forward along view Z");
+            break;
+        case "s":
+            eye[2] -= dist;
+            lookCenter[2] -= rotation;
+            console.log("translate view backward along view Z");
+            break;
+        case "q":
+            eye[1] -= dist;
+            lookCenter[1] -= rotation;
+            console.log("translate view up along view Y");
+            break;
+        case "e":
+            eye[1] += dist;
+            lookCenter[1] += rotation;
+            console.log("translate view down along view Y");
+            break;
+        case "A":
+            lookCenter[0] -= rotation;
+            console.log("rotate view left around view Y (yaw)");
+            break;
+        case "D":
+            lookCenter[0] += rotation;
+            console.log("rotate view right around view Y (yaw)");
+            break;
+        case "W":
+            lookCenter[1] += rotation;
+            console.log("rotate view forward around view X (pitch)");
+            break;
+        case "S":
+            lookCenter[1] -= rotation;
+            console.log("rotate view backward around view X (pitch)");
+            break;
+
+        // Part 4: Interactively select a model
+        // left and right — select and highlight the next/previous triangle set (previous off)
+        // space — deselect and turn off highlight
+        case "ArrowLeft":
+            if (selectedIndex === -1) {
+                selectedIndex = 0;
+            } else {
+                selectedIndex++;
+            }
+            if (selectedIndex >= numOfModels) {
+                selectedIndex = 0;
+            }
+            document.getElementById("modelIndex").innerHTML = selectedIndex;
+            console.log("select and highlight the next triangle set");
+            break;
+        case "ArrowRight":
+            if (selectedIndex === -1) {
+                selectedIndex = numOfModels-1;
+            } else {
+                selectedIndex--;
+            }
+            if (selectedIndex < 0) {
+                selectedIndex = numOfModels-1;
+            }
+            document.getElementById("modelIndex").innerHTML = selectedIndex;
+            console.log("select and highlight the previous triangle set");
+            break;
+        case " ":
+            e.preventDefault();
+            selectedIndex = -1;
+            document.getElementById("modelIndex").innerHTML = "None";
+            console.log("deselect and turn off highlight");
+            break;
+
+        // Part 5: Interactively change projection of a model
+        // < — use perspective projection (the default)
+        // = — use parallel projection
+        case ",":
+            isPerspective = true;
+            document.getElementById("mode").innerHTML = "Perspective";
+            console.log("Perspective Projection");
+            break;
+        case "=":
+            isPerspective = false;
+            document.getElementById("mode").innerHTML = "Parallel";
+            console.log("Parallel Projection");
+            break;
+
+        // Part 6: Interactively transform models
+        // k and ; — translate selection left and right along view X
+        // o and l — translate selection forward and backward along view Z
+        // i and p — translate selection up and down along view Y
+        // K and : — rotate selection left and right around view Y (yaw)
+        // O and L — rotate selection forward and backward around view X (pitch)
+        // I and P — rotate selection clockwise and counterclockwise around view Z (roll)
+        case "k":
+            if (selectedIndex !== -1) {
+                mat4.translate(inputTriangles[selectedIndex].tfMatrix, 
+                                inputTriangles[selectedIndex].tfMatrix, 
+                                new vec3.fromValues(dist, 0, 0));
+                console.log("translate selection left along view X");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case ";":
+            if (selectedIndex !== -1) {
+                mat4.translate(inputTriangles[selectedIndex].tfMatrix, 
+                                inputTriangles[selectedIndex].tfMatrix, 
+                                new vec3.fromValues(-dist, 0, 0));
+                console.log("translate selection right along view X");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case "o":
+            if (selectedIndex !== -1) {
+                mat4.translate(inputTriangles[selectedIndex].tfMatrix, 
+                                inputTriangles[selectedIndex].tfMatrix, 
+                                new vec3.fromValues(0, 0, dist));
+                console.log("translate selection forward along view Z");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case "l":
+            if (selectedIndex !== -1) {
+                mat4.translate(inputTriangles[selectedIndex].tfMatrix, 
+                                inputTriangles[selectedIndex].tfMatrix, 
+                                new vec3.fromValues(0, 0, -dist));
+                console.log("translate selection backward along view Z");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case "i":
+            if (selectedIndex !== -1) {
+                mat4.translate(inputTriangles[selectedIndex].tfMatrix, 
+                                inputTriangles[selectedIndex].tfMatrix, 
+                                new vec3.fromValues(0, dist, 0));
+                console.log("translate selection up along view Y");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case "p":
+            if (selectedIndex !== -1) {
+                mat4.translate(inputTriangles[selectedIndex].tfMatrix, 
+                                inputTriangles[selectedIndex].tfMatrix, 
+                                new vec3.fromValues(0, -dist, 0));
+                console.log("translate selection down along view Y");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case "K":
+            if (selectedIndex !== -1) {
+                mat4.rotateY(inputTriangles[selectedIndex].tfMatrix, 
+                            inputTriangles[selectedIndex].tfMatrix, 
+                            rotationAngle);
+                console.log("rotate selection left around view Y (yaw)");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case ":":
+            if (selectedIndex !== -1) {
+                mat4.rotateY(inputTriangles[selectedIndex].tfMatrix, 
+                            inputTriangles[selectedIndex].tfMatrix, 
+                            -rotationAngle);
+                console.log("rotate selection right around view Y (yaw)");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case "O":
+            if (selectedIndex !== -1) {
+                mat4.rotateX(inputTriangles[selectedIndex].tfMatrix, 
+                            inputTriangles[selectedIndex].tfMatrix, 
+                            rotationAngle);
+                console.log("rotate selection forward around view X (pitch)");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case "L":
+            if (selectedIndex !== -1) {
+                mat4.rotateX(inputTriangles[selectedIndex].tfMatrix, 
+                            inputTriangles[selectedIndex].tfMatrix, 
+                            -rotationAngle);
+                console.log("rotate selection backward around view X (pitch)");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case "I":
+            if (selectedIndex !== -1) {
+                mat4.rotateZ(inputTriangles[selectedIndex].tfMatrix, 
+                            inputTriangles[selectedIndex].tfMatrix, 
+                            rotationAngle);
+                console.log("rotate selection clockwise around view Z (roll)");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        case "P":
+            if (selectedIndex !== -1) {
+                mat4.rotateZ(inputTriangles[selectedIndex].tfMatrix, 
+                            inputTriangles[selectedIndex].tfMatrix, 
+                            -rotationAngle);
+                console.log("rotate selection counterclockwise around view Z (roll)");
+            } else {
+                console.log("No model selected!");
+            }
+            break;
+        default:
+            break;
+    }
+    requestAnimationFrame(renderTriangles);
+}
--- prog3/rasterize.js
+++ prog3/rasterize.js
@@ -6,16 +6,26 @@ const WIN_LEFT = 0; const WIN_RIGHT = 1;  // default left and right x coords in 
 const WIN_BOTTOM = 0; const WIN_TOP = 1;  // default top and bottom y coords in world space
 const INPUT_TRIANGLES_URL = "https://ncsucgclass.github.io/prog3/triangles.json"; // triangles file loc
 const INPUT_SPHERES_URL = "https://ncsucgclass.github.io/prog3/spheres.json"; // spheres file loc
-var Eye = new vec4.fromValues(0.5,0.5,-0.5,1.0); // default eye position in world space
+const INPUT_LIGHTS_URL = "https://ncsucgclass.github.io/prog3/lights.json";
+
+var inputTriangles;
+var inputLights;
+
+var eye = new vec3.fromValues(0.5, 0.5, -0.5); // default eye position in world space
+var viewUp = new vec3.fromValues(0, 1, 0);
+var lookAt = new vec3.fromValues(0, 0, 1);
+var lookCenter = new vec3.fromValues(0.5, 0.5, 0);
 
 /* webgl globals */
 var gl = null; // the all powerful gl object. It's all here folks!
-var vertexBuffer;
-var indexBuffer;
-var colorBuffer;
-var triBufferSize = 0; // the number of indices in the triangle buffer
-var vertexPositionAttrib; // where to put position for vertex shader
-var vertexColorAttrib;
+var shaderProgram;
+var isPerspective = true;
+var vertexBuffer = [];
+var indexBuffer = [];
+var normalBuffer = [];
+var setCenters = [];
+var selectedIndex = -1;
+var scaling = new vec3.fromValues(1.2, 1.2, 1.2);
 
 // ASSIGNMENT HELPER FUNCTIONS
 
@@ -60,9 +70,10 @@ function setupWebGL() {
         gl.clearColor(0.0, 0.0, 0.0, 1.0); // use black when we clear the frame buffer
         gl.clearDepth(1.0); // use max when we clear the depth buffer
         gl.enable(gl.DEPTH_TEST); // use hidden surface removal (with zbuffering)
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
       }
     } // end try
-    
     catch(e) {
       console.log(e);
     } // end catch
@@ -71,49 +82,50 @@ function setupWebGL() {
 
 // read triangles in, load them into webgl buffers
 function loadTriangles() {
-    var inputTriangles = getJSONFile(INPUT_TRIANGLES_URL,"triangles");
+    inputTriangles = getJSONFile(INPUT_TRIANGLES_URL,"triangles");
     if (inputTriangles != String.null) { 
-        var whichSetVert; // index of vertex in current triangle set
-        var whichSetTri; // index of triangle in current triangle set
-        var coordArray = []; // 1D array of vertex coords for WebGL
-        var indexArray = [];
-        var colorArray = [];
-        var tri = vec3.create();
-        var indexOffset = vec3.create();
-        var vertexBufferSize = 0;
-
         for (var whichSet=0; whichSet<inputTriangles.length; whichSet++) {
-            vec3.set(indexOffset, vertexBufferSize, vertexBufferSize, vertexBufferSize);
+            var whichSetVert; // index of vertex in current triangle set
+            var whichSetTri; // index of triangle in current triangle set
+            var coordArray = []; // 1D array of vertex coords for WebGL
+            var indexArray = [];
+            var normalArray = [];
+            var triCenter = vec3.create();
             // set up the vertex coord array
-            for (whichSetVert=0; whichSetVert<inputTriangles[whichSet].vertices.length; whichSetVert++) {
-                var coord = inputTriangles[whichSet].vertices[whichSetVert]
-                var color = inputTriangles[whichSet].material.diffuse;
+            for (whichSetVert = 0; whichSetVert<inputTriangles[whichSet].vertices.length; whichSetVert++) {
+                var coord = inputTriangles[whichSet].vertices[whichSetVert];
+                var norm = inputTriangles[whichSet].normals[whichSetVert];
                 coordArray.push(coord[0], coord[1], coord[2]);
-                colorArray.push(color[0], color[1], color[2]);
+                normalArray.push(norm[0], norm[1], norm[2]);
                 // console.log(inputTriangles[whichSet].vertices[whichSetVert]);
             }
             for (whichSetTri = 0; whichSetTri < inputTriangles[whichSet].triangles.length; whichSetTri++) {
-                vec3.add(tri, indexOffset, inputTriangles[whichSet].triangles[whichSetTri]);
+                var tri = inputTriangles[whichSet].triangles[whichSetTri];
                 indexArray.push(tri[0], tri[1], tri[2]);
+                for (var i=0; i<3;i++) {
+                    vec3.scaleAndAdd(triCenter, triCenter, inputTriangles[whichSet].vertices[tri[i]], 1/3);
+                }
             }
-            vertexBufferSize += inputTriangles[whichSet].vertices.length;
-            triBufferSize += inputTriangles[whichSet].triangles.length;
+            setCenters[whichSet] = vec3.create();
+            vec3.scale(setCenters[whichSet], triCenter, 1/inputTriangles[whichSet].triangles.length);
+
+            // send the vertex coords to webGL
+            vertexBuffer[whichSet] = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer[whichSet]); // activate that buffer
+            gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(coordArray),gl.STATIC_DRAW); // coords to that buffer
+
+            // send triangle indices to webGL
+            indexBuffer[whichSet] = gl.createBuffer(); // init empty triangle index buffer
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer[whichSet]); // activate that buffer
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexArray), gl.STATIC_DRAW); // indices to that buffer
+            indexBuffer[whichSet].bufferSize = inputTriangles[whichSet].triangles.length * 3;
+
+            normalBuffer[whichSet] = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer[whichSet]);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normalArray), gl.STATIC_DRAW);
+
+            inputTriangles[whichSet].tfMatrix = mat4.create();
         } // end for each triangle set 
-        triBufferSize *= 3;
-        // console.log(coordArray.length);
-        // send the vertex coords to webGL
-        vertexBuffer = gl.createBuffer(); // init empty vertex coord buffer
-        gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer); // activate that buffer
-        gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(coordArray),gl.STATIC_DRAW); // coords to that buffer
-
-        colorBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorArray), gl.STATIC_DRAW);
-
-        // send triangle indices to webGL
-        indexBuffer = gl.createBuffer(); // init empty triangle index buffer
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer); // activate that buffer
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexArray), gl.STATIC_DRAW); // indices to that buffer
     } // end if triangles found
 } // end load triangles
 
@@ -122,20 +134,51 @@ function setupShaders() {
     
     // define fragment shader in essl using es6 template strings
     var fShaderCode = `
-        varying lowp vec3 fragColor;
+        precision mediump float;
+        uniform vec3 lightPos;
+        uniform vec3 lightAmbient;
+        uniform vec3 lightDiffuse;
+        uniform vec3 lightSpecular;
+        uniform vec3 eyePos;
+        
+        uniform vec3 materialAmbient;
+        uniform vec3 materialDiffuse;
+        uniform vec3 materialSpecular;
+        uniform float materialN;
+
+        varying vec3 mVertexPos;
+        varying vec3 mVertexNorm;
+
         void main(void) {
-            gl_FragColor = vec4(fragColor, 1.0); // all fragments are white
+            vec3 lightDir = normalize(lightPos - mVertexPos);
+            vec3 viewDir = normalize(eyePos - mVertexPos);
+            vec3 normNorm = normalize(mVertexNorm);
+            vec3 H = normalize(lightDir + viewDir);
+            float diffuseFactor = max(dot(normNorm, lightDir), 0.0);
+            float specularFactor = pow(max(0.0, dot(H, normNorm)), materialN);
+
+            vec3 color = lightAmbient * materialAmbient + 
+                            lightDiffuse * materialDiffuse * diffuseFactor + 
+                            lightSpecular * materialSpecular * specularFactor;
+
+            gl_FragColor = vec4(color, 1.0);
         }
     `;
     
     // define vertex shader in essl using es6 template strings
     var vShaderCode = `
-        attribute vec3 vertexPosition;
-        attribute vec3 vertexColor;
-        varying lowp vec3 fragColor;
+        attribute vec3 vertexPos;
+        attribute vec3 vertexNorm;
+        uniform mat4 mMatrix;
+        uniform mat4 pOrOMatrix;
+        uniform mat4 vMatrix;
+        uniform mat3 nMatrix;
+        varying vec3 mVertexPos;
+        varying vec3 mVertexNorm;
         void main(void) {
-            gl_Position = vec4(vertexPosition, 1.0); // use the untransformed position
-            fragColor = vertexColor;
+            gl_Position = pOrOMatrix * vMatrix * mMatrix * vec4(vertexPos, 1.0);
+            mVertexPos = vec3(mMatrix * vec4(vertexPos, 1.0));
+            mVertexNorm = nMatrix * vertexNorm;
         }
     `;
     
@@ -157,7 +200,7 @@ function setupShaders() {
             throw "error during vertex shader compile: " + gl.getShaderInfoLog(vShader);  
             gl.deleteShader(vShader);
         } else { // no compile errors
-            var shaderProgram = gl.createProgram(); // create the single shader program
+            shaderProgram = gl.createProgram(); // create the single shader program
             gl.attachShader(shaderProgram, fShader); // put frag shader in program
             gl.attachShader(shaderProgram, vShader); // put vertex shader in program
             gl.linkProgram(shaderProgram); // link program into gl context
@@ -166,11 +209,25 @@ function setupShaders() {
                 throw "error during shader program linking: " + gl.getProgramInfoLog(shaderProgram);
             } else { // no shader program link errors
                 gl.useProgram(shaderProgram); // activate shader program (frag and vert)
-                vertexPositionAttrib = // get pointer to vertex shader input
-                    gl.getAttribLocation(shaderProgram, "vertexPosition"); 
-                gl.enableVertexAttribArray(vertexPositionAttrib); // input to shader from array
-                vertexColorAttrib = gl.getAttribLocation(shaderProgram, "vertexColor");
-                gl.enableVertexAttribArray(vertexColorAttrib); // input to shader from array
+                // get pointer to vertex shader input
+                shaderProgram.vertexPos = gl.getAttribLocation(shaderProgram, "vertexPos");
+                shaderProgram.vertexNorm = gl.getAttribLocation(shaderProgram, "vertexNorm");
+
+                shaderProgram.mMatrix = gl.getUniformLocation(shaderProgram, "mMatrix");
+                shaderProgram.vMatrix = gl.getUniformLocation(shaderProgram, "vMatrix");
+                shaderProgram.pOrOMatrix = gl.getUniformLocation(shaderProgram, "pOrOMatrix");
+                shaderProgram.nMatrix = gl.getUniformLocation(shaderProgram, "nMatrix");
+
+                shaderProgram.lightPos = gl.getUniformLocation(shaderProgram, "lightPos");
+                shaderProgram.lightAmbient = gl.getUniformLocation(shaderProgram, "lightAmbient");
+                shaderProgram.lightDiffuse = gl.getUniformLocation(shaderProgram, "lightDiffuse");
+                shaderProgram.lightSpecular = gl.getUniformLocation(shaderProgram, "lightSpecular");
+                shaderProgram.eyePos = gl.getUniformLocation(shaderProgram, "eyePos");
+
+                shaderProgram.materialAmbient = gl.getUniformLocation(shaderProgram, "materialAmbient");
+                shaderProgram.materialDiffuse = gl.getUniformLocation(shaderProgram, "materialDiffuse");
+                shaderProgram.materialSpecular = gl.getUniformLocation(shaderProgram, "materialSpecular");
+                shaderProgram.materialN = gl.getUniformLocation(shaderProgram, "materialN");
             } // end if no shader program link errors
         } // end if no compile errors
     } // end try 
@@ -182,29 +239,81 @@ function setupShaders() {
 
 // render the loaded model
 function renderTriangles() {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // clear frame/depth buffers
-    
-    // vertex buffer: activate and feed into vertex shader
-    gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer); // activate
-    gl.vertexAttribPointer(vertexPositionAttrib,3,gl.FLOAT,false,0,0); // feed
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    loadLights();
+    gl.uniform3fv(shaderProgram.eyePos, eye);
+    // Viewing Matrix
+    var vMatrix = mat4.create();
+    mat4.lookAt(vMatrix, eye, lookCenter, viewUp);
+    // Perspective Matrix
+    var pOrOMatrix = mat4.create();
+    if (isPerspective) {
+        mat4.perspective(pOrOMatrix, Math.PI/2, gl.viewportWidth/gl.viewportHeight, 0.1, 100);
+    } else {
+        mat4.ortho(pOrOMatrix, -1.0, 1.0, -1.0, 1.0, 0.1, 100);
+    }
+    gl.uniformMatrix4fv(shaderProgram.vMatrix, false, vMatrix);
+    gl.uniformMatrix4fv(shaderProgram.pOrOMatrix, false, pOrOMatrix);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer); // activate
-    gl.vertexAttribPointer(vertexColorAttrib, 3, gl.FLOAT, false, 0, 0) // feed
+    for (var whichSet=0; whichSet<inputTriangles.length; whichSet++) {
+        var material = inputTriangles[whichSet].material;
+        gl.uniform3fv(shaderProgram.materialAmbient, material.ambient);
+        gl.uniform3fv(shaderProgram.materialDiffuse, material.diffuse);
+        gl.uniform3fv(shaderProgram.materialSpecular, material.specular);
+        gl.uniform1f(shaderProgram.materialN, material.n);
 
-    // triangle buffer: activate and render
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,indexBuffer); // activate
-    gl.drawElements(gl.TRIANGLES, triBufferSize, gl.UNSIGNED_SHORT,0); // render
-    
+        // Model Matrix
+        var mMatrix = mat4.create();
+        mat4.translate(mMatrix, mMatrix, setCenters[whichSet]);
+        if (whichSet === selectedIndex) {
+            mat4.scale(mMatrix, mMatrix, scaling);
+        }
+        mat4.multiply(mMatrix, mMatrix, inputTriangles[whichSet].tfMatrix);
+        mat4.translate(mMatrix, mMatrix, vec3.negate(vec3.create(), setCenters[whichSet]));
+        gl.uniformMatrix4fv(shaderProgram.mMatrix, false, mMatrix);
+
+        // Normal Transform Matrix
+        var nMatrix = mat3.create();
+        mat3.normalFromMat4(nMatrix, mMatrix);
+        // smooth shading with vertex normals
+        mat3.invert(nMatrix, nMatrix);
+        mat3.transpose(nMatrix, nMatrix);
+        gl.uniformMatrix3fv(shaderProgram.nMatrix, false, nMatrix);
+
+        // vertex buffer: activate and feed into vertex shader
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer[whichSet]); // activate
+        gl.vertexAttribPointer(shaderProgram.vertexPos, 3, gl.FLOAT, false, 0, 0); // feed
+        gl.enableVertexAttribArray(shaderProgram.vertexPos); // input to shader from array
+
+        // normal buffer: activate and feed into vertex shader
+        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer[whichSet]); // activate
+        gl.vertexAttribPointer(shaderProgram.vertexNorm, 3, gl.FLOAT, false, 0, 0); // feed
+        gl.enableVertexAttribArray(shaderProgram.vertexNorm);
+
+        // index buffer: activate and feed into vertex shader
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer[whichSet]); // activate
+        gl.drawElements(gl.TRIANGLES, indexBuffer[whichSet].bufferSize, gl.UNSIGNED_SHORT, 0); // render
+    }
+
+
 } // end render triangles
 
+function loadLights() {
+    inputLights = getJSONFile(INPUT_LIGHTS_URL,"lights");
+    var light = inputLights[0];
+    gl.uniform3f(shaderProgram.lightPos, light.x, light.y, light.z);
+    gl.uniform3fv(shaderProgram.lightAmbient, light.ambient);
+    gl.uniform3fv(shaderProgram.lightDiffuse, light.diffuse);
+    gl.uniform3fv(shaderProgram.lightSpecular, light.specular);
+}
 
 /* MAIN -- HERE is where execution begins after window load */
 
 function main() {
-  
-  setupWebGL(); // set up the webGL environment
-  loadTriangles(); // load in the triangles from tri file
-  setupShaders(); // setup the webGL shaders
-  renderTriangles(); // draw the triangles using webGL
+    setupWebGL(); // set up the webGL environment
+    loadTriangles(); // load in the triangles from tri file
+    setupShaders(); // setup the webGL shaders
+    document.addEventListener("keydown", onKeydown);
+    renderTriangles(); // draw the triangles using webGL
   
 } // end main

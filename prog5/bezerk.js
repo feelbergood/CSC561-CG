757e7e172b2e108f39f1f1e2c71a5b02dcc1cd1e
Yudong RAO <liamrao@gmail.com>
2019-12-06 16:30:13 -0500

    finished part 3 and most of part 4

--- prog5/bezerk.js
+++ prog5/bezerk.js
@@ -1,7 +1,7 @@
 // Utility functions
 // Check collision between an object and a list of objects
 function collide(obj1, obj2) {
-    var originPoint = player.position.clone();
+    var originPoint = obj1.position.clone();
 	for (var vtx of obj1.geometry.vertices)
 	{		
 		var localVertex = vtx.clone();
@@ -15,15 +15,51 @@ function collide(obj1, obj2) {
         } else { // If obj2 is an object
             collisionResults = ray.intersectObject( obj2 );
         }
-		if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) 
-			return true;
+        if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) {
+			return true; 
+        }
     }
     return false;
 }
 
+function moveBetween(robot, start, end, dir)
+{
+    if (dir) { // dir is true => move vertically
+        if (robot.position.y >= end) {
+            robot.direction = [0, -1];
+        }
+        if (robot.position.y <= start) {
+            robot.direction = [0, 1];
+        }
+        if (robot.direction) {
+            robot.position.y += robot.direction[1] * robotSpeed;
+        }
+    } else { // dir is false => move horizontally
+        if (robot.position.x >= end) {
+            robot.direction = [-1, 0];
+        }
+        if (robot.position.x <= start) {
+            robot.direction = [1, 0];
+        }
+        if (robot.direction) {
+            robot.position.x += robot.direction[0] * robotSpeed;
+        }
+    }
+}
+
+// Global variables
+var gameOver = false;
+var score = 0;
+var direction = [0, 0];
+var playerSpeed = 0.1;
+var robotSpeed = 0.03;
+var bulletSpeed = 0.1;
+var robotBulletSpeed = 0.05;
+
 // Threejs setup
 var scene = new THREE.Scene();
 // var camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
+// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var camera = new THREE.PerspectiveCamera( 75, 4 / 3, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
 // renderer.setSize( window.innerWidth, window.innerHeight );
@@ -32,21 +68,25 @@ document.body.appendChild( renderer.domElement );
 
 // Init player
 var playerGeometry = new THREE.BoxGeometry( 0.2, 0.4, 0.2 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var player = new THREE.Mesh( playerGeometry, material );
+var playerMaterial = new THREE.MeshBasicMaterial( { color: 0xdeb887 } );
+var player = new THREE.Mesh( playerGeometry, playerMaterial );
 
 // Init walls
-var wallMaterial = new THREE.MeshBasicMaterial( { color: 0x663300 } );
+var wallMaterial = new THREE.MeshBasicMaterial( { color: 0x6a5acd } );
 
 var leftWall = new THREE.Mesh( new THREE.BoxGeometry( 0.1, 6, 0.2 ), wallMaterial );
 leftWall.position.x = -4;
+
 var rightWall = new THREE.Mesh( new THREE.BoxGeometry( 0.1, 6, 0.2 ), wallMaterial );
 rightWall.position.x = 4;
+
 var topWall = new THREE.Mesh( new THREE.BoxGeometry( 8, 0.1, 0.2 ), wallMaterial );
 topWall.position.y = 3;
+
 var bottomWallLeft = new THREE.Mesh( new THREE.BoxGeometry( 3, 0.1, 0.2 ), wallMaterial );
 bottomWallLeft.position.x = -2;
 bottomWallLeft.position.y = -3;
+
 var bottomWallRight = new THREE.Mesh( new THREE.BoxGeometry( 3, 0.1, 0.2 ), wallMaterial );
 bottomWallRight.position.x = 2;
 bottomWallRight.position.y = -3;
@@ -65,10 +105,50 @@ insideWall3.position.y = -1.5;
 
 var walls = [leftWall, rightWall, topWall, bottomWallLeft, bottomWallRight, insideWall1, insideWall2, insideWall3];
 
+// Init bullets
+var bulletGeometry = new THREE.SphereGeometry( 0.05, 10, 10 );
+// var bulletGeometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
+var bulletMaterial = new THREE.MeshBasicMaterial( { color: 0xdeb887 } );
+var bullets = [];
+
+// Init robots
+var robotGeometry = new THREE.BoxGeometry( 0.1, 0.4, 0.2 );
+var robotMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+var robots = [];
+
+var robot1 = new THREE.Mesh( robotGeometry, robotMaterial );
+robot1.position.x = -3;
+robot1.position.y = 0;
+robot1.bullets = [];
+robots.push(robot1);
+
+var robot2 = new THREE.Mesh( robotGeometry, robotMaterial );
+robot2.position.x = 3;
+robot2.position.y = 0;
+robot2.bullets = [];
+robots.push(robot2);
+
+var robot3 = new THREE.Mesh( robotGeometry, robotMaterial );
+robot3.position.x = -1;
+robot3.position.y = 2;
+robot3.bullets = [];
+robots.push(robot3);
+
+var robot4 = new THREE.Mesh( robotGeometry, robotMaterial );
+robot4.position.x = -3;
+robot4.position.y = -2;
+robot4.bullets = [];
+robots.push(robot4);
+
 // Add meshes to scene
 for (var wall of walls) {
     scene.add(wall);
 }
+
+for (var robot of robots) {
+    scene.add( robot );
+}
+
 scene.add( player );
 
 // Adjust camera
@@ -76,9 +156,51 @@ camera.position.z = 5;
 
 // Game loop
 var animate = function () {
-    if (collide(player, walls)) {
-        console.log("Hit wall!");
+    // Collision between player and wall
+    if (collide(player, walls) || collide(player, robots)) {
+        gameOver = true;
+        document.getElementById("status").style.display = "block";
     }
+    
+    // Collision between wall and bullet
+    for (var blt of bullets) {
+        for (var r of robots) {
+            if (collide(r, blt)) {
+                scene.remove(blt);
+                if (bullets.indexOf(blt) > -1) {
+                    bullets.splice(bullets.indexOf(blt), 1);
+                }
+                scene.remove(r);
+                if (robots.indexOf(r) > -1) {
+                    robots.splice(robots.indexOf(r), 1);
+                }
+                score += 100;
+                console.log(score);
+            }
+        }
+        if (collide(blt, walls)) {
+            console.log("bullet collides wall");
+            setTimeout(() => {
+                scene.remove(blt);
+                if (bullets.indexOf(blt) > -1) {
+                    bullets.splice(bullets.indexOf(blt), 1);
+                }
+            }, 1000);
+        } else {
+            blt.position.x += blt.direction[0] * bulletSpeed;
+            blt.position.y += blt.direction[1] * bulletSpeed;
+        }
+    }
+
+    if (!gameOver) {
+        moveBetween(robot1, 0, 2, true);
+        moveBetween(robot2, 0, 2, true);
+        moveBetween(robot3, -1, 1, false);
+        moveBetween(robot4, -3, 3, false);
+        for (var rbt of robots) {
+        }
+    }
+
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 };
--- prog5/input.js
+++ prog5/input.js
@@ -1,28 +1,40 @@
-var dist = 0.1;
-var rotation = 0.01;
-var rotationAngle = Math.PI / 20;
-
 function onKeydown(e) {
-    var key = e.key.toLowerCase();
-    switch(key) {
-        // a and d — translate view left and right along view X
-        // w and s — translate view forward and backward along view Z
-        // q and e — translate view up and down along view Y
-        // A and D — rotate view left and right around view Y (yaw)
-        // W and S — rotate view forward and backward around view X (pitch)
-        case "w":
-            player.position.y += dist;
-            break;
-        case "a":
-            player.position.x -= dist;
-            break;
-        case "s":
-            player.position.y -= dist;
-            break;
-        case "d":
-            player.position.x += dist;
-            break;
-        default:
-            break;
+    if (!gameOver) {
+        var key = e.key.toLowerCase();
+        switch(key) {
+            // a and d — translate view left and right along view X
+            // w and s — translate view forward and backward along view Z
+            // q and e — translate view up and down along view Y
+            // A and D — rotate view left and right around view Y (yaw)
+            // W and S — rotate view forward and backward around view X (pitch)
+            case "w":
+                player.position.y += playerSpeed;
+                direction = [0, 1];
+                break;
+            case "a":
+                player.position.x -= playerSpeed;
+                direction = [-1, 0];
+                break;
+            case "s":
+                player.position.y -= playerSpeed;
+                direction = [0, -1];
+                break;
+            case "d":
+                player.position.x += playerSpeed;
+                direction = [1, 0];
+                break;
+            case " ":
+                if (direction[0] !== 0 || direction[1] !== 0) {
+                    console.log("fire!");
+                    var newBullet = new THREE.Mesh( bulletGeometry, bulletMaterial );
+                    newBullet.position.x = player.position.x;
+                    newBullet.position.y = player.position.y;
+                    newBullet.direction = direction;
+                    bullets.push(newBullet);
+                    scene.add(newBullet);
+                }
+            default:
+                break;
+        }
     }
 }
